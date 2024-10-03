7270ab87b78c8716916ccc0ce6c5e95d10364766
zhiller <1187303661@qq.com>
2024-10-03 17:09:39 +0800

    update

--- MathLib/src/main/java/io/zhiller/mathlib/MathUtils.kt
+++
@@ -1,8 +0,0 @@
-package io.zhiller.mathlib
-
-object MathUtils {
-  fun calcPlus(x:Int,y:Int):Int = x+y
-  fun calcMinus(x:Int,y:Int):Int = x-y
-  fun calcMulti(x:Int,y:Int):Int = x*y
-  fun calcDivide(x:Int,y:Int):Int = x/y
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# zer-android-libs
-Android开发中常用到的库，发布到jitpack上面让他自己构建
